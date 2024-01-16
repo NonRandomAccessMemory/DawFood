18a9e4bd26bdf19deb186aa977d3fb39afc549e7
snavgar217 <snavgar217@g.educaand.es>
2024-01-16 11:17:38 +0100

    FIRST COMMIT START INICIO

---
+++ src/main/java/DawFoodSV/Infraestructure/DB_Productos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DawFoodSV.Infraestructure;
+
+import DawFoodSV.Domain.Producto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author snavgar
+ */
+public class DB_Productos {
+    /*TODO:
+       IMPLEMENTAR INTERFACE IDB*/
+    
+    /*EMULO SER UNA BASE DE DATOS*/
+    private ArrayList<Producto> m_BaseDeDatos= new ArrayList<Producto>();
+
+}
