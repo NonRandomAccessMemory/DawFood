3c7a0018f00c061113eb6eea788914ca4ad23e9d
vfergon346@g.educaand.es <vfergon346@g.educaand.es>
2024-01-16 12:48:52 +0100

    Menú bienvenida

--- src/main/java/DawFoodSV/DawFood.java
+++ src/main/java/DawFoodSV/DawFood.java
@@ -4,6 +4,8 @@
 
 package DawFoodSV;
 
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -12,8 +14,50 @@ package DawFoodSV;
 public class DawFood {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
-
+        String mensaje = """
+                      
+                         Bienvenidos a DawFood
+                      
+                      """;
+        
+        JOptionPane.showInputDialog(null, mensaje);
+        
+        String[] opcionesMenu1 = {
+            "1-. Cofigura tu menú",
+            "2-. Salir",
+            "3-. Otras opciones de Adimn",};
+        JOptionPane.showMessageDialog(null, opcionesMenu1);
+        String opcionElegida;
+        boolean continuar = true;
+         do {
+        
+        opcionElegida = (String) JOptionPane.showInputDialog(null,
+                "Elige una opción", "DawFood",
+                JOptionPane.QUESTION_MESSAGE, null,
+                opcionesMenu1, "1-. Cofigura tu menú");
+        switch (opcionElegida) {
+            case "1-. Cofigura tu menú" -> {
+                 
+                 
+            break;
+        }
+            
+            case "2-. Salir" -> {
+                continuar = false;
+                break;
+            }
+            case "3-. Otras opciones de Adimn"-> {
+                
+            break;
+            }
+            default -> {
+                continuar = false;
+            }
+            }
+        
+        
+        } while(continuar);
+        
     }
 }
