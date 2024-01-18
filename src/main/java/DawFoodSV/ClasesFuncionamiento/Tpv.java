fda5735d46e4c49e41d5fd8f3f02e0e872ccae14
snavgar217 <snavgar217@g.educaand.es>
2024-01-18 10:43:11 +0100

    Añadida clase TPV y E_Usuario

---
+++ src/main/java/DawFoodSV/ClasesFuncionamiento/Tpv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package DawFoodSV.ClasesFuncionamiento;
+
+/**
+ *
+ * @author snavgar
+ */
+public enum Tpv {
+    /*Añadir Numero de serie Unico*/
+    
+    /*Añadir llamada al menu principal*/
+    
+}
